b94c3af73e8f6ef9771935030921edeccdaadd6d
Nihler <vanilor97@gmail.com>
2024-04-30 16:29:25 +0200

    connecting to db + fetching/adding posts

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require("./models/post");
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://jacob:u3QzvQOPzwquVBIV@cluster0.wqdapct.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 //parsing request to json
 app.use(bodyParser.json());
@@ -21,29 +35,31 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save();
   res.status(201).json({
     message: "Post added",
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "12312",
-      title: "First server side post",
-      content: "This is coming from server",
-    },
-    {
-      id: "2352",
-      title: "First server side post n2",
-      content: "This is coming from server!",
-    },
-  ];
+  const posts = Post.find().then((docs) => {
+    console.log(docs);
+    res.status(200).json({
+      message: "Post fetched succesfully!",
+      posts: docs,
+    });
+  });
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  const id = req.params.id;
+  console.log(id);
   res.status(200).json({
-    message: "Post fetched succesfully!",
-    posts: posts,
+    message: "Post deleted",
   });
 });
 
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,6 +2,7 @@ import { Post } from './post.model';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
@@ -12,11 +13,22 @@ export class PostsService {
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: Post[] }>(
-        'http://localhost:3000/api/posts'
+      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+      .pipe(
+        map((postData) => {
+          return postData.posts.map(
+            (post: { title: string; content: string; _id: string }) => {
+              return {
+                title: post.title,
+                content: post.content,
+                id: post._id,
+              };
+            }
+          );
+        })
       )
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
 
@@ -28,7 +40,7 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = { id: 0, title: title, content: content };
+    const post: Post = { id: '0', title: title, content: content };
     this.http
       .post<{ message: string }>('http://localhost:3000/api/posts', post)
       .subscribe((resData) => {
