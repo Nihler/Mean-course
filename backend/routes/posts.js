805bd8814e8265dfa0240a4b845a23a59d352fc5
Nihler <vanilor97@gmail.com>
2024-05-26 13:41:22 +0200

    checking user for update and delete

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -92,12 +92,13 @@ router.get("/:id", (req, res, next) => {
 
 //deleting post
 router.delete("/:id", checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    console.log(result);
-    res.status(200).json({
-      message: "Post deleted",
-    });
-  });
+  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(
+    (result) => {
+      if (result.deletedCount > 0)
+        res.status(200).json({ message: "Post deleted" });
+      else res.status(401).json({ message: "Unauthorized update" });
+    }
+  );
 });
 
 //updating post
@@ -117,9 +118,13 @@ router.put(
       content: req.body.content,
       imagePath: imagePath,
     });
-    Post.updateOne({ _id: req.params.id }, post).then((result) => {
-      console.log(result);
-      res.status(200).json({ message: "Update successfull" });
+    Post.updateOne(
+      { _id: req.params.id, creator: req.userData.userId },
+      post
+    ).then((result) => {
+      if (result.modifiedCount > 0)
+        res.status(200).json({ message: "Update successfull" });
+      else res.status(401).json({ message: "Unauthorized update" });
     });
   }
 );
